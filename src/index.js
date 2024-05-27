1b57343087cf5abfa63de3abda9aeaafe16253b2
cristinavelo <cristinavelo1@gmail.com>
2024-05-27 20:37:24 +0200

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generatePoem(event) {
   let apiKey = "79fc46bb4abo220adbf7a3a2et15fe6f";
   let prompt = `User instructions: Generate an italian poem about ${instructionsElement.value}`;
   let context =
-    "You are a romantic poem expert and like to write shorts poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Display only the poem. Make sure to follow the user instructions. Sign at the bottom of the poem with 'SheCodes AI' inside a <strong> element";
+    "You are a romantic poem expert and like to write shorts poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Display only the poem. Make sure to follow the user instructions. Sign in a new line at the bottom of the poem with 'SheCodes AI' inside a <strong> element";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generating an Italian poem about ${instructionsElement.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
