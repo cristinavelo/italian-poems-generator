7e97cd33a9f0977150df7398c2a6b79910b57984
cristinavelo <cristinavelo1@gmail.com>
2024-05-28 14:52:42 +0200

    Fixed js to the new topic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-function displayPoem(response) {
+function displayQuote(response) {
   console.log("Poem generated");
 
-  new Typewriter("#poem", {
+  new Typewriter("#quote", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -9,26 +9,26 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateQuote(event) {
   event.preventDefault();
 
   let instructionsElement = document.querySelector("#user-instructions");
   let apiKey = "79fc46bb4abo220adbf7a3a2et15fe6f";
-  let prompt = `User instructions: Generate an italian poem about ${instructionsElement.value}`;
+  let prompt = `User instructions: Generate an italian quote about ${instructionsElement.value}`;
   let context =
-    "You are a romantic poem expert and like to write shorts poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Display only the poem. Make sure to follow the user instructions. Sign in a new line at the bottom of the poem with 'SheCodes AI' inside a <strong> element";
+    "You are a inspirational life quote expert and like to write shorts quotes. Your mission is to generate a 2 line quote NOT in bold in basic HTML and separate each line with a <br />. Display only the quote. Make sure to follow the user instructions. Sign in a new line at the bottom of the quote with 'SheCodes AI' inside a <strong> element";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink">Generating an Italian poem about ${instructionsElement.value}</div>`;
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="blink">Generating an Italian quote about ${instructionsElement.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayQuote);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
